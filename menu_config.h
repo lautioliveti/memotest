314a73773d6915d0fd76ca3df28f1459b6601cc0
lautaro oliveti <125168699+lautioliveti@users.noreply.github.com>
2026-02-10 18:55:51 -0300

    Delete menu_config.h

--- menu_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_CONFIG_H_INCLUDED
-#define MENU_CONFIG_H_INCLUDED
-
-// Funciones de menú de configuración
-int mostrarMenuConfiguracion(SDL_Renderer *renderer, SDL_Texture *texturaFondo, Configuracion *config);
-int solicitarNombresJugadores(SDL_Renderer *renderer, SDL_Texture *texturaFondo, DatosJugadores *jugadores, int cantJugadores);
-
-#endif // MENU_CONFIG_H_INCLUDED
